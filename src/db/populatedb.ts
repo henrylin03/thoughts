3525d562781565bd6f2257db6d93fbb4b0e8414c
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-09 15:51:18 +1100

    feat(populatedb): add script to populate db
    fix incorrect path in package.json script

---
+++ src/db/populatedb.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+import dotenv from "dotenv";
+import { Client } from "pg";
+
+dotenv.config();
+
+const main = async () => {
+	const DATABASE_NAME = "members_only";
+	const CREATE_TABLES_QUERY = `
+CREATE TABLE IF NOT EXISTS users (
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    username text NOT NULL UNIQUE,
+    password text NOT NULL,
+    is_member boolean NOT NULL DEFAULT false,
+    is_admin boolean NOT NULL DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    author_id integer REFERENCES users(id),
+    title varchar(70) NOT NULL,
+    text varchar(255) NOT NULL,
+    timestamp TIMESTAMP DEFAULT now()
+);
+`;
+
+	const localDatabaseUrl = `postgresql://${process.env.USER_NAME}:${process.env.PASSWORD}@localhost:5432/${DATABASE_NAME}`;
+	console.log("seeding...");
+	const client = new Client({
+		connectionString: localDatabaseUrl,
+	});
+
+	await client.connect();
+	await client.query(CREATE_TABLES_QUERY);
+	await client.end();
+	console.log("done");
+};
+
+main();
