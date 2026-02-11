5a8621bfdaaac1d4eb219318ef838ed12ed52255
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-11 15:14:30 +1100

    feat(loginForm): display error message depending on the error type.
    this is done at the controller level to minimise logic in the ejs. it also means we don't need to set a local.__ variable for the error message, because it's just grabbed from the req.session.flash.error.

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -2,6 +2,18 @@ import type { Request, Response } from "express";
 import passport from "@/config/passport.js";
 
 type LoginField = "username" | "password";
+type LoginError = {
+	field: LoginField;
+	message: string;
+};
+
+const getLoginErrorMessage = (fieldWithError: LoginField): string => {
+	if (fieldWithError === "username")
+		return "Sorry, we couldn't find an account with that username.";
+	if (fieldWithError === "password")
+		return "Sorry, that password isn't right. Please try again.";
+	else return "";
+};
 
 const loginGet = async (req: Request, res: Response) => {
 	console.log("req.session:", req.session);
@@ -11,11 +23,13 @@ const loginGet = async (req: Request, res: Response) => {
 		return res.render("pages/loginForm");
 
 	const fieldWithError: LoginField = session.flash.error.pop();
-	console.log(fieldWithError);
+	const loginError: LoginError = {
+		field: fieldWithError,
+		message: getLoginErrorMessage(fieldWithError),
+	};
 
 	req.session.flash.error = [];
-
-	res.render("pages/loginForm");
+	res.render("pages/loginForm", { error: loginError });
 };
 
 const loginPost = passport.authenticate("local", {
