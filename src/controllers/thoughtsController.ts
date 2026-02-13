38df79cb0b8d204ebc7fcabc27a025de1a7082b1
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-13 17:32:36 +1100

    feat(thoughts): timestamp from database is parsed into time _since_ posting (eg 1h = 1hour, 2d = 2 days etc)
    used dayjs library for this to reduce custom code and thus maintenance overhead

--- src/controllers/thoughtsController.ts
+++ src/controllers/thoughtsController.ts
@@ -1,10 +1,37 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime.js";
+import updateLocale from "dayjs/plugin/updateLocale.js";
 import type { Request, Response } from "express";
 import { matchedData, validationResult } from "express-validator";
 import { addThought, getAllThoughts } from "@/db/queries.js";
 import { validateNewThought } from "@/helpers/validation.js";
 
+dayjs.extend(updateLocale);
+dayjs.extend(relativeTime);
+
+dayjs.updateLocale("en", {
+	relativeTime: {
+		s: "a few seconds",
+		m: "1m",
+		mm: "%dm",
+		h: "1h",
+		hh: "%dh",
+		d: "1d",
+		dd: "%dd",
+		M: "1mth",
+		MM: "%dmth",
+		y: "1y",
+		yy: "%dy",
+	},
+});
+
 const allThoughtsGet = async (_req: Request, res: Response) => {
-	const thoughts = await getAllThoughts();
+	const fetchedThoughts = await getAllThoughts();
+	const thoughts = fetchedThoughts.map((thought) => {
+		const { timestamp, ...attributes } = thought;
+		return { ...attributes, timeSincePosting: dayjs(timestamp).toNow(true) };
+	});
+
 	res.render("pages/allThoughts", { thoughts });
 };
 
